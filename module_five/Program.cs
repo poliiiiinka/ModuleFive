b82d409483e5e7827c7afe33717bb12dce6b3176
Полина Артамонова <poliiiiinka71@gmail.com>
2024-03-27 14:16:27 +0300

    области видимости

--- module_five/Program.cs
+++ module_five/Program.cs
@@ -5,69 +5,23 @@ namespace module_five
 {
     internal class Program
     {
+        static int num1 = 667;
         static void Main(string[] args)
         {
-            // задание 5.3.13
+            Console.WriteLine(num1);
+            Console.WriteLine(Sum());
 
-            int[] arraysort = new int[] { -9, 5, 64, 37, -54, 0 };
-            SortArray(arraysort, out arraysort, out arraysort);
-        }
-
-        // module 5, unit 3
-        // задание 5.3.13
-        static int[] GetArrayFromConsole(int num)
-        {
-            var result = new int[num];
-
-            for (int i = 0; i < result.Length; i++)
+            if (num1 > 0)
             {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
+                var num3 = 5;
+                Console.WriteLine(num1 + num3);
             }
-            return result;
         }
 
-        static void SortArray(int[] array, out int[] sorteddesc, out int[] sortedasc)
+        static int Sum()
         {
-            sorteddesc = SortArrayDesc(array);
-            Console.WriteLine();
-            sortedasc = SortArrayAsc(array);
-        }
-
-        // сортировка массива от большего к меньшему
-        static int[] SortArrayDesc(int[] result)
-        {
-            int temp = 0;
-            for (int i = 0; i < result.Length; i++)
-                for (int j = i + 1; j < result.Length; j++)
-                    if (result[i] < result[j])
-                    {
-                        temp = result[i];
-                        result[i] = result[j];
-                        result[j] = temp;
-                    }
-            foreach (int i in result)
-                Console.WriteLine(i);
-
-            return result;
-        }
-
-        // сортировка массива от меньшего к большему
-        static int[] SortArrayAsc(int[] result)
-        {
-            int temp = 0;
-            for (int i = 0; i < result.Length; i++)
-                for (int j = i + 1; j < result.Length; j++)
-                    if (result[i] > result[j])
-                    {
-                        temp = result[i];
-                        result[i] = result[j];
-                        result[j] = temp;
-                    }
-            foreach (int i in result)
-                Console.WriteLine(i);
-
-            return result;
+            var num2 = 1;
+            return num1 + num2;
         }
     }
 
